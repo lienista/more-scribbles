f17c4b59527f9cef52b08c96af9fff2091c4c6de
Lienne Nguyen <lienista@Liens-MacBook-Pro.local>
2014-01-02 21:54:24 -0800

    Initial Commit

---
+++ Neighbor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Neighbor
+//
+//  Created by Lien Nguyen on 1/2/14.
+//  Copyright (c) 2014 Lienne Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
